aa847b43c4aa0454f1f671a8dcae22c61b1a9046
gihannazmy <gihanelsayed187@gmail.com>
2024-09-06 16:14:32 +0300

    add project_detail

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -4,7 +4,7 @@ from account.models import User
 
 
 # Create your models here.
-# ================================================================
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -18,23 +18,20 @@ class Project(models.Model):
     total_target = models.FloatField()
     start_time = models.DateTimeField(default=timezone.now)
     end_time = models.DateTimeField(default=timezone.now)
-    rate = models.IntegerField(default=0)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name="category"
-    )
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
-    created_at = models.DateTimeField(auto_now_add=True)
+    # rate = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
 
 
 class Donation(models.Model):
-    donation = models.FloatField()
-    project = models.ForeignKey(
-        Project, on_delete=models.CASCADE, related_name="donation_project"
-    )
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="donation_user"
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
+    amount = models.FloatField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='donation_project')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='donation_user')
+    created_at = models.DateTimeField(timezone.now())
+
+    def __str__(self):
+        return str(self.amount)
