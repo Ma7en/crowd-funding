453c21dce7ecdbda55cb75f9d1520299084e770c
Ma7en <m9ee9m@gmail.com>
2024-09-07 00:08:09 +0300

    edit config prettier

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,57 +1,63 @@
-from django.shortcuts import render ,get_object_or_404,redirect
-from . models import Project , Category
-from . forms import ProjectForm , CategoryForm
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Project, Category
+from .forms import ProjectForm, CategoryForm
 
 
 def project_list(request):
     projects = Project.objects.all()
-    return render(request, 'projects/project_list.html', {'projects': projects})
+    return render(request, "projects/project_list.html", {"projects": projects})
+
 
 def project_detail(request, id):
     project = get_object_or_404(Project, id=id)
-    return render(request, 'projects/project_detail.html', {'project': project})
+    return render(request, "projects/project_detail.html", {"project": project})
+
 
 def create_project(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProjectForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('project_list')
+            return redirect("project_list")
     else:
         form = ProjectForm()
-    return render(request, 'projects/create/create.html', {'form': form})
+    return render(request, "projects/create/create.html", {"form": form})
+
 
 def edit_project(request, project_id):
     project = get_object_or_404(Project, id=project_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProjectForm(request.POST, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('project_detail', project_id=project_id)
+            return redirect("project_detail", project_id=project_id)
     else:
         form = ProjectForm(instance=project)
-    return render(request, 'edit_project.html', {'form': form, 'project': project})
+    return render(request, "edit_project.html", {"form": form, "project": project})
+
 
 def category_list(request):
     categories = Category.objects.all()
-    return render(request, 'categories/category_list.html', {'categories': categories})
+    return render(request, "categories/category_list.html", {"categories": categories})
+
 
 def create_category(request):
     category_list = Category.objects.all()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CategoryForm(request.POST)
-        new_category = request.POST['name']
+        new_category = request.POST["name"]
         for category in category_list:
             if category_list.name == new_category:
-                error = 'Name not valid!'
-                return render(request , 'categories/create/create.html',context={'form': form, 'error': error})
-
+                error = "Name not valid!"
+                return render(
+                    request,
+                    "categories/create/create.html",
+                    context={"form": form, "error": error},
+                )
 
         form.save()
-        return redirect('category.list')
+        return redirect("category.list")
     else:
         form = CategoryForm()
-    return render(request, 'categories/create/create.html', {'form': form})
-
+    return render(request, "categories/create/create.html", {"form": form})
