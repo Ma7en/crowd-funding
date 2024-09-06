3cccfb28aba60abe646060fde2661205f20ef259
Ma7en <m9ee9m@gmail.com>
2024-09-06 19:43:49 +0300

    edit login

--- crowdfunding/account/urls.py
+++ crowdfunding/account/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("profile/delete/<int:pk>", DeleteAccount.as_view(), name="delete_user"),
     path("logout/", Logout, name="logout"),
     path("", include("django.contrib.auth.urls")),
-    path("socialauth/", include("allauth.urls")),
+    path("oauth/", include("social_django.urls"), name="social"),
 ]
