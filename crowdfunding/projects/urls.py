cd4f9631d4a53b28821c969ca1a2b240db79d722
gihannazmy <gihanelsayed187@gmail.com>
2024-09-06 16:38:47 +0300

    adding urls&views

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -1,3 +1,9 @@
+
 from django.urls import path
 
-urlpatterns = []
+from projects.views import project_detail, project_list
+
+urlpatterns = [
+    path('projects/', project_list, name='project_list'),
+    path('projects/<int:id>/', project_detail, name='project_detail'),
+]
