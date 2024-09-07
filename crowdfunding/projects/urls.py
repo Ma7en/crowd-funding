960ee382bcc865de81e6406307fc3d05839761ca
Ma7en <m9ee9m@gmail.com>
2024-09-07 13:24:42 +0300

    fix login and signup bugs

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -12,12 +12,12 @@ from projects.views import (
 )
 
 urlpatterns = [
-    path("projects/", project_list, name="project_list"),
-    path("projects/<int:id>/", project_detail, name="project_detail"),
-    path("projects/delete/<int:id>/", delete, name="project_delete"),
-    path("projects/create", create_project, name="project_create"),
-    path("projects/edit/<int:id>", edit_project, name="edit_project"),
+    path("", project_list, name="project_list"),
+    path("<int:id>", project_detail, name="project_detail"),
+    path("delete/<int:id>", delete, name="project_delete"),
+    path("create", create_project, name="project_create"),
+    path("edit/<int:id>", edit_project, name="edit_project"),
     path("category/", category_list, name="category.list"),
-    path("category/<str:category>/", category_detail, name="category_detail"),
+    path("category/<str:category>", category_detail, name="category_detail"),
     path("category/create", create_category, name="category_create"),
 ]
