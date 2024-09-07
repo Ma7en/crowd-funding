acac3ffd925436d61cc9fcbf96820388c1b98fd4
gihannazmy <gihanelsayed187@gmail.com>
2024-09-07 09:43:25 +0300

    added edit.html

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -1,17 +1,13 @@
 from django.urls import path
 
-from projects.views import (
-    project_detail,
-    project_list,
-    create_project,
-    category_list,
-    create_category,
-)
+from projects.views import project_detail, project_list, create_project, edit_project, delete
 
 urlpatterns = [
-    path("", project_list, name="project_list"),
-    path("<int:id>/", project_detail, name="project_detail"),
-    path("create-project", create_project, name="project_create"),
-    path("category", category_list, name="category.list"),
-    path("create-category", create_category, name="category.create"),
+    path('projects/', project_list, name='project_list'),
+    path('projects/<int:id>/', project_detail, name='project_detail'),
+    path('projects/delete/<int:id>/', delete, name='project_delete'),
+    path('projects/create', create_project, name='project_create'),
+    path('projects/edit/<int:id>', edit_project, name='edit_project'),
+    # path('category/' , category_list , name='category.list'),
+    # path('category/create' , create_category , name='category.create'),
 ]
