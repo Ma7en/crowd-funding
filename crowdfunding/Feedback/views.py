219dfcbc1ba558db268120bb2f9bccffd5b43810
Ma7en <m9ee9m@gmail.com>
2024-09-13 01:49:26 +0300

    edit profile

--- crowdfunding/Feedback/views.py
+++ crowdfunding/Feedback/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import  render, redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from .forms import ReportForm, ReviewForm
 from .models import Report, Review
@@ -6,37 +6,31 @@ from projects.models import Project
 from django.views import generic
 
 
-
 def report_list(request):
     reports = Report.objects.all()
-    return render(request, 'reports/reports_list.html', {'reports': reports})
+    return render(request, "reports/reports_list.html", {"reports": reports})
 
 
 class ReportProject(generic.CreateView):
     model = Report
     form_class = ReportForm
-    success_url = reverse_lazy('report_list')
-    template_name='reports/create.html'
-
-    
+    success_url = reverse_lazy("report_list")
+    template_name = "reports/create.html"
 
 
-def report_project(request, id):
-    if request.method == 'POST':
+def report_project(request, pk):
+    if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
             review = form.save(commit=False)
-            project = Project.objects.get(id=id)
+            project = Project.objects.get(id=pk)
             review.project = project
             review.user = request.user
             review.save()
-            return redirect('project_detail', id=id)
+            return redirect("project_detail", id=pk)
     else:
         form = ReviewForm()
-    return render(request, 'feedback/create_review.html', {'form': form})
-
-    
-
+    return render(request, "feedback/create_review.html", {"form": form})
 
 
 # def report_project(request):
