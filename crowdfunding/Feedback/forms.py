c8b22e564abd43c6fa6312cb914839632cca2fcc
MichaelAwadEissa <michaelawadeissa@gmail.com>
2024-09-13 02:12:11 +0300

    Rport Form , Template , Views

--- crowdfunding/Feedback/forms.py
+++ crowdfunding/Feedback/forms.py
@@ -1,12 +1,31 @@
 from django import forms
-from .models import Review, Report
+from .models import Review,Report, Comment
 
-class  ReviewForm(forms.ModelForm):
+
+
+
+class ReviewForm(forms.ModelForm):
+    
+
     class Meta:
         model = Review
-        fields = ['review', 'rate']
+        fields = ['review','rate']
+
+
+
+
+class CommentForm(forms.ModelForm):
+
+	class Meta:
+		model = Comment
+		fields = ['content']
+
+
+
 
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ['reason', 'status']
+        fields = ['reason','status']
+
+    
