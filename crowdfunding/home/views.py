3b14c2d2ca22326ce1a5cb0c4af57ec9a1b1e79c
MichaelAwadEissa <michaelawadeissa@gmail.com>
2024-09-12 22:25:16 +0300

    adding project

--- crowdfunding/home/views.py
+++ crowdfunding/home/views.py
@@ -5,26 +5,24 @@ from django.http import HttpResponse
 from django.views import generic
 from projects.models import Project
 
-
 def index(request):
-    return render(request, "home/home.html")
-
+    projects = Project.objects.all()
+    print(projects)
+    return render(request, 'home/home.html', {'projects': projects})
 
 def contact(request):
-    return render(request, "home/contact.html")
-
+    return render(request, 'home/contact.html')
 
 def about(request):
-    return render(request, "home/about.html")
-
+    return render(request, 'home/about.html')
 
 class SearchView(generic.ListView):
-    template_name = "projects/project_list.html"
+    template_name = 'projects/project_list.html'
     model = Project
-    context_object_name = "projects"
+    context_object_name = 'projects'
 
     def get_queryset(self):
-        param = self.request.GET.get("param")
+        param = self.request.GET.get('param')
         projects = Project.objects.filter(title__icontains=param)
-        self.extra_context = {"search": param}
+        self.extra_context={'search':param}
         return projects
